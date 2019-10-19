fbfb047c142c6b79daa8f7b15742045b533d8590
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-19 02:47:27 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,13 +15,19 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
-    
+
     def current_user
       User.find_by(id: session[:user_id])
     end
 
     def logged_in?
       !!current_user
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect to('/login')
+      end
     end
   end
 
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,18 +1,51 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    if logged_in?
-      @tweets = Tweet.all
-      erb :'tweets/tweets'
+    redirect_if_not_logged_in
+    @tweets = Tweet.all
+    erb :'tweets/tweets'
+  end
+
+  post '/tweets' do
+    if params[:content].empty?
+      redirect to('/tweets/new')
     else
-      redirect to('/login')
+      tweet = current_user.tweets.new(params)
+
+      tweet.save
+      redirect to("/tweets/#{tweet.id}")
     end
   end
 
   get '/tweets/new' do
-    # if logged_in?
-      erb :'tweets/new'
-    # end
+    redirect_if_not_logged_in
+    erb :'tweets/new'
+  end
+
+  get '/tweets/:id' do
+    redirect_if_not_logged_in
+    @tweet = Tweet.find_by(id: params[:id])
+    erb :'tweets/show'
+  end
+
+  patch '/tweets/:id' do
+    tweet = Tweet.find_by(id: params[:id])
+    if tweet && tweet.user == current_user
+      tweet.update(params[:tweet])
+      tweet.save
+    else
+      redirect to("/tweets/#{tweet.id}")
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    redirect_if_not_logged_in
+    @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        erb :'tweets/edit'
+      else
+        redirect to '/tweets'
+      end
   end
 
 
